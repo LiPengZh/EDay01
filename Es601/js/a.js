a3c66dc6a88401d84680acdfd1eebc560d4fd0d0
xiaoxiao <111>
2018-12-31 22:33:12 +0800

    增加了ES6

---
+++ Es601/js/a.js
@@ -0,0 +1,11 @@
+let a=10;
+alert(a);
+
+
+// show()=>{
+//     alert("hello")
+// }
+let fu =()=>{
+    alert("123");
+}
+fu();
---
+++ Es601/lib/a.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var a = 10;
+alert(a);
+
+// show()=>{
+//     alert("hello")
+// }
+var fu = function fu() {
+    alert("123");
+};
+fu();
